1f000c380fd5d5987690f78786fb1c21d527d83e
Nick Vegean <nick.vegean@naluri.life>
2024-06-02 10:12:33 +0800

    chore: refactor

--- src/routes/upload.rs
+++ src/routes/upload.rs
@@ -2,89 +2,34 @@ use actix_multipart::{Field, Multipart};
 use actix_web::{post, web, Responder};
 use cpu_time::ProcessTime;
 use futures_util::TryStreamExt;
-use s3::error::SdkError;
-use s3::operation::put_object::{PutObjectError, PutObjectOutput};
-use s3::primitives::ByteStream;
-use std::io::{BufWriter, Cursor};
-
-use std::path::Path;
-
 use image::codecs::jpeg::JpegEncoder;
 use image::io::Reader as ImageReader;
 use image::{ExtendedColorType, ImageEncoder};
+use std::io::{BufWriter, Cursor};
+
+use s3::error::SdkError;
+use s3::operation::put_object::{PutObjectError, PutObjectOutput};
+use s3::primitives::ByteStream;
 
 use fast_image_resize::images::Image;
 use fast_image_resize::{IntoImageView, ResizeAlg, ResizeOptions, Resizer};
 
 use aws_sdk_s3 as s3;
 
-async fn show_buckets(strict: bool, client: &s3::Client, region: &str) -> Result<(), s3::Error> {
-    let resp = client.list_buckets().send().await?;
-    let buckets = resp.buckets();
-    let num_buckets = buckets.len();
-
-    let mut in_region = 0;
-
-    for bucket in buckets {
-        if strict {
-            let r = client
-                .get_bucket_location()
-                .bucket(bucket.name().unwrap_or_default())
-                .send()
-                .await?;
-
-            if r.location_constraint().unwrap().as_ref() == region {
-                println!("{}", bucket.name().unwrap_or_default());
-                in_region += 1;
-            }
-        } else {
-            println!("{}", bucket.name().unwrap_or_default());
-        }
-    }
-
-    println!();
-    if strict {
-        println!(
-            "Found {} buckets in the {} region out of a total of {} buckets.",
-            in_region, region, num_buckets
-        );
-    } else {
-        println!("Found {} buckets in all regions.", num_buckets);
-    }
-
-    Ok(())
-}
-
-async fn upload_object(
-    client: &s3::Client,
-    bucket_name: &str,
-    buffer: Vec<u8>,
-    key: &str,
-) -> Result<PutObjectOutput, SdkError<PutObjectError>> {
-    let body = ByteStream::from(buffer);
-    client
-        .put_object()
-        .bucket(bucket_name)
-        .key(key)
-        .body(body)
-        .send()
-        .await
-}
-
 // avg: ~26ms with Nearest algorithm
 #[post("/upload")]
 pub async fn upload_to_s3(mut payload: Multipart, client: web::Data<s3::Client>) -> impl Responder {
-    show_buckets(false, &client, "us-east-1")
-        .await
-        .expect("Listing buckets failed");
+    // NOTE: i just use this to test connection to aws cli
+    // show_buckets(false, &client, "us-east-1")
+    //     .await
+    //     .expect("Listing buckets failed");
 
     let start = ProcessTime::try_now().expect("Getting process time failed");
 
     while let Ok(Some(field)) = payload.try_next().await {
-        println!("Processing field: {:?}", field);
-        let buffer = process_image(field).await.expect("Processing image failed");
+        let (buffer, file_name) = process_image(field).await.expect("Processing image failed");
         let put_object_output =
-            upload_object(&client, "image-compress-tournament", buffer, "test.jpg").await;
+            upload_object(&client, "image-compress-tournament", buffer, &file_name).await;
 
         match put_object_output {
             Ok(_) => println!("Object uploaded successfully"),
@@ -93,16 +38,24 @@ pub async fn upload_to_s3(mut payload: Multipart, client: web::Data<s3::Client>)
     }
 
     let elapsed_time = start.try_elapsed().expect("Getting elapsed time failed");
-    println!("Elapsed time: {:?}", elapsed_time);
+    println!(
+        "Total time: {:?} for thread: {:?}",
+        elapsed_time,
+        std::thread::current().id()
+    );
 
     "ok"
 }
 
-async fn process_image(mut field: Field) -> Result<Vec<u8>, Box<dyn std::error::Error>> {
+async fn process_image(mut field: Field) -> Result<(Vec<u8>, String), Box<dyn std::error::Error>> {
     let mut data = web::BytesMut::new();
 
-    let file_name = field.content_disposition().get_filename().unwrap();
-    println!("Uploading file: {}", file_name);
+    // TODO: do we want to keep the original file name?
+    let file_name = field
+        .content_disposition()
+        .get_filename()
+        .unwrap()
+        .to_owned();
 
     // let final_path = PathBuf::from(constants::DEST).join(format!(
     //     "{}-{}.jpg",
@@ -118,17 +71,11 @@ async fn process_image(mut field: Field) -> Result<Vec<u8>, Box<dyn std::error::
         data.extend_from_slice(&chunk);
     }
 
-    let start_reader = ProcessTime::try_now().expect("Getting process time failed");
     let img = ImageReader::new(Cursor::new(data))
         .with_guessed_format()?
         .decode()?;
 
-    let elapsed_time_reader = start_reader
-        .try_elapsed()
-        .expect("Getting elapsed time failed");
-    println!("Reading time: {:?}", elapsed_time_reader);
-
-    let start = ProcessTime::try_now().expect("Getting process time failed");
+    // let start = ProcessTime::try_now().expect("Getting process time failed");
 
     let dst_width = 1024;
     let dst_height = 768;
@@ -165,14 +112,71 @@ async fn process_image(mut field: Field) -> Result<Vec<u8>, Box<dyn std::error::
         )
         .expect("Writing image failed");
 
-    let elapsed_time = start.try_elapsed().expect("Getting elapsed time failed");
-    println!("Processing time: {:?}", elapsed_time);
+    // let elapsed_time = start.try_elapsed().expect("Getting elapsed time failed");
+    // println!("Processing time: {:?}", elapsed_time);
 
     // std::fs::write(final_path, result_buf.get_ref()).expect("Writing file failed");
+    let image_data = (
+        result_buf
+            .into_inner()
+            .expect("Getting inner buffer failed"),
+        file_name,
+    );
 
-    Ok(result_buf
-        .into_inner()
-        .expect("Getting inner buffer failed"))
+    Ok(image_data)
+}
+
+async fn upload_object(
+    client: &s3::Client,
+    bucket_name: &str,
+    buffer: Vec<u8>,
+    key: &str,
+) -> Result<PutObjectOutput, SdkError<PutObjectError>> {
+    let body = ByteStream::from(buffer);
+    client
+        .put_object()
+        .bucket(bucket_name)
+        .key(key)
+        .body(body)
+        .send()
+        .await
+}
+
+async fn _show_buckets(strict: bool, client: &s3::Client, region: &str) -> Result<(), s3::Error> {
+    let resp = client.list_buckets().send().await?;
+    let buckets = resp.buckets();
+    let num_buckets = buckets.len();
+
+    let mut in_region = 0;
+
+    for bucket in buckets {
+        if strict {
+            let r = client
+                .get_bucket_location()
+                .bucket(bucket.name().unwrap_or_default())
+                .send()
+                .await?;
+
+            if r.location_constraint().unwrap().as_ref() == region {
+                println!("{}", bucket.name().unwrap_or_default());
+                in_region += 1;
+            }
+        } else {
+            println!("{}", bucket.name().unwrap_or_default());
+        }
+    }
+
+    println!();
+    if strict {
+        println!(
+            "Found {} buckets in the {} region out of a total of {} buckets.",
+            in_region, region, num_buckets
+        );
+    } else {
+        println!("Found {} buckets in all regions.", num_buckets);
+    }
+
+    Ok(())
 }
 
 //let mut elapsed_times = Vec::new();
